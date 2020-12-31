cae8540a63abba2dad827603a2a20e15c6ed1a86
francofantini <franco@fantini.dev>
2020-12-30 23:40:49 -0300

    feat(Meta): Adds favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,8 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Eco</title>
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+                <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
             </Head>
             <DarkMode />
             {/* eslint-disable-next-line react/jsx-props-no-spreading */}
