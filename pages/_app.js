11b49e762c6311888df030ac0b0fdbb487b79c98
francofantini <franco@fantini.dev>
2020-12-31 00:16:45 -0300

    feat(Meta): Adds meta description.

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <title>Eco</title>
+                <meta name="description" content="Test your REST client's requests with a single echo endpoint" />
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
                 <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
